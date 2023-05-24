226049128884af557a76e9473afb1aa7259ad679
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-24 12:03:06 +0530

    Update Content

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = (enteredGoalText) => {
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}]);
-  }
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  };
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler}/>
-      <FlatList data={courseGoals} keyExtractor={(item, index) => item.id} renderItem={itemData => {
-        return (
-          <GoalItem text={itemData.item.text} />
-        )
-      }} >
-      </FlatList>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={(itemData) => {
+          return <GoalItem text={itemData.item.text} />;
+        }}
+      ></FlatList>
     </View>
   );
 }
@@ -28,9 +38,9 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   goalContainer: {
     flex: 4,
-  }
+  },
 });
